8acba394163db1a91f7ecb84075c0bf6b2c5d5c0
Andy Jandl <akjandl@gmail.com>
2019-01-23 22:11:38 -0600

    POC html diff generation
    - used example found online to generate pretty html diff from two img files

--- test.js
+++ test.js
@@ -1,16 +1,30 @@
 var tesseract = require('./node_modules/tesseract.js');
-let longImage = './much_longer.png';
-let shortImage = './test.png';
+let oscarGood = './OscarGood.png';
+let oscarBad = './OscarBad.png';
+let diff_match_patch = require('./node_modules/diff-match-patch/index.js');
 
-// first image
-tesseract.recognize(longImage)
+let goodText = 'goodText text';
+let badText = 'badText text';
+let dmp = new diff_match_patch();
+
+tesseract.recognize(oscarGood)
   .then(e => console.log('first image'))
-  .then(result => console.log(result.text))
+  .then(result => {
+  	goodText = result.text;
+  })
 ;
 
-// second image
-tesseract.recognize(shortImage)
+tesseract.recognize(oscarBad)
   .then(e => console.log('second image'))
-  .then(result => console.log(result.text))
-  .finally(e => tesseract.terminate())
+  .then(result => {
+  	badText = result.text;
+  })
+  .finally(e => {
+    // example from: https://neil.fraser.name/software/diff_match_patch/demos/diff.html
+  	let diff = dmp.diff_main(goodText, badText);
+  	dmp.diff_cleanupSemantic(diff);
+  	console.log(dmp.diff_prettyHtml(diff));
+
+  	tesseract.terminate()
+  })
 ;
