ad8eb86bbe46aa77f2ea94800264cda044b23be9
Andy Jandl <akjandl@gmail.com>
2019-02-10 13:40:12 -0600

    working POC app, diff two hard coded images

--- src/App.js
+++ src/App.js
@@ -1,65 +1,56 @@
 import React, { Component } from 'react';
-import tesseract from '../node_modules/tesseract.js';
+// import tesseract from '../node_modules/tesseract.js';  // server side import
 import diff_match_patch from '../node_modules/diff-match-patch/index.js';
 
 import OscarBad from './assets/images/OscarGood.png';
 import OscarGood from './assets/images/OscarBad.png'
+const tesseract = window.Tesseract;  // imported in index.html (required for client side execution)
 
 
 class App extends Component {
 
-  makeDiffHtml = (img1, img2) => {
-
-    let oscarGoodPath = './assets/images/OscarGood.png';
-    let oscarBadPath = './assets/images/OscarBad.png';
-
-    let promise1 = new Promise(function (resolve, reject) {
-
-      let goodText = null;
-      let badText = null;
-      let dmp = new diff_match_patch();
-
-      tesseract.recognize(OscarBad)
-        .then(e => console.log('first image'))
-        .then(result => {
-          goodText = result.text;
-        })
-      ;
-
-      tesseract.recognize(OscarGood)
-        .then(e => console.log('second image'))
-        .then(result => {
-          badText = result.text;
-        })
-        .finally(e => {
-          // example from: https://neil.fraser.name/software/diff_match_patch/demos/diff.html
-          let diff = dmp.diff_main(goodText, badText);
-          dmp.diff_cleanupSemantic(diff);
-          resolve(dmp.diff_prettyHtml(diff));
-
-          tesseract.terminate()
-        });
-      });
-    return promise1;
+  state = {
+    img1Text: null,
+    img2Text: null
   };
 
-     // async thing;
-     // resolve(async_thing_value);
-  returnDiffVal = () => {
-    this.makeDiffHtml().then(function(value) {
-      console.log(value);
-      // expected output: "foo"
-    });
+  textFromImg = (image, stateKey) => {
+    this.setState({[stateKey]: null});
+    tesseract.recognize(image)
+    .then(result => {
+      this.setState({[stateKey]: result.text});
+    })
+  };
+
+  getTextFromImages = (img1, img2) => {
+    this.textFromImg(img1, 'img1Text');
+    this.textFromImg(img2, 'img2Text');
+  };
+
+  diffText = (text1, text2) => {
+    let dmp = new diff_match_patch();
+    let diff = dmp.diff_main(text1, text2);
+    dmp.diff_cleanupSemantic(diff);
+    return dmp.diff_prettyHtml(diff);
   };
 
   render() {
+
+    let diffHtml = null;
+    if (this.state.img1Text && this.state.img2Text) {
+      let rawHtml = null;
+      rawHtml = this.diffText(this.state.img1Text, this.state.img2Text);
+      console.log(rawHtml);
+      diffHtml = rawHtml;
+    }
 
     return (
       <React.Fragment>
         <h1>This is the App component</h1>
         <img src={OscarBad} />
         <img src={OscarGood} />
-        <button onClick={() => this.returnDiffVal()}>GO!</button>
+        <button onClick={() => this.getTextFromImages(OscarGood, OscarBad)}>GO!</button>
+        <div dangerouslySetInnerHTML={{ __html: diffHtml}} />
       </React.Fragment>
     );
   };
